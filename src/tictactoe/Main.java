2bce5b80cb4b1da1d981fd7c5611ce2b4f8a5787
Pavel German <kudastrelyat@gmail.com>
2020-12-01 22:51:56 +0500

    4. Вывод поля в консоль. Проинициализированное поле готово. Теперь необходимо вывести его в консоль. 5. Тест программы. Можно запускать первый тест программы.

---
+++ src/tictactoe/Main.java
@@ -0,0 +1,40 @@
+package tictactoe;
+
+public class Main {
+    public static char[][] map;
+    public static final int SIZE = 3;
+    public static final int DOTS_TO_WIN = 3;
+
+    public static final char DOT_EMPTY = '•';
+    public static final char DOT_X = 'X';
+    public static final char DOT_O = 'O';
+
+
+    public static void initMap() {
+        map = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                map[i][j] = DOT_EMPTY;
+            }
+        }
+    }
+
+    public static void printMap() {
+        for (int i = 0; i <= SIZE; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        initMap();
+        printMap();
+    }
+}
